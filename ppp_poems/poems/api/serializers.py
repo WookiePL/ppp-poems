9f97b0806b1e31fa0ae67ea0d90af07b50cf2e5b
Dominik <homik777@wp.pl>
2017-12-28 03:10:12 +0100

    Changes in data model: 1. Poem has user 2. Rate has user
    Changes in serializers:
    1. Retrieving poem's average rating instead of rate set
    2. Retrieving poem's user as nested json

--- ppp_poems/poems/api/serializers.py
+++ ppp_poems/poems/api/serializers.py
@@ -1,7 +1,15 @@
+from django.contrib.auth.models import User
+from django.db.models import Avg
 from rest_framework import serializers, permissions
 from oauth2_provider.models import Application
 
-from poems.models import Author, Poem
+from poems.models import Author, Poem, Rate
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username')
 
 
 class AuthorSerializer(serializers.ModelSerializer):
@@ -11,9 +19,17 @@ class AuthorSerializer(serializers.ModelSerializer):
 
 
 class PoemSerializer(serializers.ModelSerializer):
+    user = UserSerializer(many=False, read_only=True)
+    rating = serializers.SerializerMethodField()
+
     class Meta:
         model = Poem
-        fields = ('id', 'title', 'description', 'content', 'rates', 'author', 'creation_time', 'modification_time')
+        fields = ('id', 'title', 'description', 'content', 'author', 'creation_time', 'modification_time',
+                  'user', 'rating')
+
+    @staticmethod
+    def get_rating(obj):
+        return sum(c.rating for c in obj.rates.all()) / len(obj.rates.all())
 
 
 class ApplicationSerializer(serializers.ModelSerializer):
