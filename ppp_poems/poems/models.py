be4063703ad295b2b36f63ab4d45919fe0d8abcf
Dominik <homik777@wp.pl>
2018-02-05 21:37:53 +0100

    Changes in rating model

--- ppp_poems/poems/models.py
+++ ppp_poems/poems/models.py
@@ -11,23 +11,10 @@ class Author(models.Model):
         return f"{self.name} {self.surname}"
 
 
-class Rate(models.Model):
-    user = models.ForeignKey(User, null=False)
-    rating = models.IntegerField(validators=[
-        MinValueValidator(1),
-        MaxValueValidator(5)
-    ])
-    creation_time = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.rating}"
-
-
 class Poem(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     content = models.TextField(blank=True)
-    rates = models.ManyToManyField(Rate, blank=True)
     author = models.ForeignKey(Author, null=True)
     user = models.ForeignKey(User, null=False)
     creation_time = models.DateTimeField(auto_now_add=True)
@@ -39,6 +26,19 @@ class Poem(models.Model):
 
     def __str__(self):
         return f"Poem: {self.title}"
+
+
+class Rate(models.Model):
+    user = models.ForeignKey(User, null=False)
+    poem = models.ForeignKey(Poem, null=False)
+    rating = models.IntegerField(validators=[
+        MinValueValidator(1),
+        MaxValueValidator(5)
+    ])
+    creation_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.rating}"
 
 
 class Comment(models.Model):
